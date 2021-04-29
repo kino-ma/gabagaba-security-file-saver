f8696f36d78a1b9b46bb4e6a318371d9af20cc34
kino-ma <kino.ma.ms@gmail.com>
2021-04-29 23:41:26 +0900

    [feat] make HTML

--- index.php
+++ index.php
@@ -1,20 +1,29 @@
-<?php
-    $content = file_get_contents('php://input');
-    $success = false;
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>File Saver</title>
+    </head>
+    <body>
+        <?php
+            $content = file_get_contents('php://input');
+            $success = false;
 
-    if ($content) {
-        mkdir("./out", 777, true);
-        $bytes_written = file_put_contents(__DIR__ . "/out/data.txt", $content);
+            if ($content) {
+                mkdir("./out", 777, true);
+                $bytes_written = file_put_contents(__DIR__ . "/out/data.txt", $content);
 
-        $success = $bytes_written > 0;
-    }
+                $success = $bytes_written > 0;
+            }
 
-    if ($success) {
-        echo "write success\n";
-        echo $content;
-        echo "\n";
-    } else {
-        echo "failed";
-    }
-?>
-
+            if ($success) {
+                echo "write success\n";
+                echo "<code>"
+                echo $content;
+                echo "</code>"
+                echo "\n";
+            } else {
+                echo "failed";
+            }
+        ?>
+    </body>
+</html>
