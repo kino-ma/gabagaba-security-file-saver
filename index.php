628c3579828ebfe7692555a196d6a6560366ae7e
kino-ma <kino.ma.ms@gmail.com>
2021-04-29 23:37:14 +0900

    [feat] God Commit

--- index.php
+++ index.php
@@ -1,8 +1,20 @@
 <?php
     $content = file_get_contents('php://input');
+    $success = false;
 
     if ($content) {
         mkdir("./out", 777, true);
-        file_put_contents(__DIR__ . "./out/data.txt", $content);
+        $bytes_written = file_put_contents(__DIR__ . "/out/data.txt", $content);
+
+        $success = $bytes_written > 0;
+    }
+
+    if ($success) {
+        echo "write success\n";
+        echo $content;
+        echo "\n";
+    } else {
+        echo "failed";
     }
 ?>
+
